1de6c78f8ff9fb3443313de1830d4f5610e4a582
Jaaromy Zierse <jaaromy.zierse@observian.com>
2018-12-18 11:23:31 -0700

    Create modules and namespaces. Use class for DB

--- app.py
+++ app.py
@@ -1,6 +1,31 @@
-from passlib.context import CryptContext
-from tinydb import TinyDB, Query
+from model.security import pwd_context as Context
+from model.db import DB
 import argparse
+
+db = DB()
+
+
+def startup():
+    Context.load_path("./crypt.ini")
+
+
+def eval_hash_from_user(user, password):
+    res = db.get_user(user)
+    hsh = None
+
+    if res:
+        hsh = res["hash"]
+
+    ok, new_hash = Context.verify_and_update(password, hsh)
+
+    if ok and new_hash:
+        db.update_hash(args.user, new_hash)
+
+    return ok
+
+
+if __name__ == '__main__':
+    startup()
 
 parser = argparse.ArgumentParser(description="Securely process and store passwords")
 parser.add_argument("-u", "--user", help="User name", dest="user", required=True)
@@ -9,47 +34,9 @@ parser.add_argument("-n", "--new", help="Create new user", dest="new", action="s
 parser.add_argument("-a", "--admin", help="Is admin user", dest="admin", action="store_true")
 args = parser.parse_args()
 
-db = TinyDB('./db/pass.json')
-user_table = db.table("user")
-Context = Query()
-User = Query()
-
-pass_ctx = CryptContext.from_path("./crypt.ini")
-
-
-def eval_hash_from_user(user, password):
-    res = user_table.get(User.name == user)
-    hsh = None
-
-    if res:
-        hsh = res["hash"]
-
-    ok, new_hash = pass_ctx.verify_and_update(password, hsh)
-
-    if ok and new_hash:
-        update_hash(args.user, new_hash)
-
-    return ok
-
-
-def update_hash(user, new_hash):
-    user_table.upsert({"name": user, "hash": new_hash}, User.name == user)
-    print("Hash Updated")
-
-
-def create_user(user, password, admin):
-    if admin:
-        category = "admin"
-    else:
-        category = None
-
-    n_hash = pass_ctx.hash(password, category=category)
-    update_hash(user, n_hash)
-    print("User created successfully")
-
 
 if args.new:
-    create_user(args.user, args.password, args.admin)
+    db.create_user(args.user, args.password, args.admin)
     exit(0)
 
 valid = eval_hash_from_user(args.user, args.password)
@@ -58,4 +45,3 @@ if valid:
     print("Login Success!")
 else:
     print("Login Failed!")
-
